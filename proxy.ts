547de825233b5d5608b63e8839c4a3d25f9843c8
Kit <k_hutcheson@hotmail.co.uk>
2026-07-13 20:49:18 +0100

    Exclude images folder from auth middleware

--- proxy.ts
+++ proxy.ts
@@ -64,5 +64,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|images|favicon.ico).*)'],
 }
